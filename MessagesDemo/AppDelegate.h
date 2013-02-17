08bcc98b65b9f8c835514706003b8feb54457154
Jesse Squires <jesse.d.squires@gmail.com>
2013-02-17 11:29:16 -0500

    Initial Commit

---
+++ MessagesDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MessagesDemo
+//
+//  Created by Jesse Squires on 2/17/13.
+//  Copyright (c) 2013 Hexed Bits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MessagesDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MessagesDemo
+//
+//  Created by Jesse Squires on 2/17/13.
+//  Copyright (c) 2013 Hexed Bits. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
