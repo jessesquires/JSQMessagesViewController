73e8a64656f7c9663c9e44caa2e5a2cad0d16490
Jesse Squires <jesse.d.squires@gmail.com>
2013-07-26 12:48:30 -0400

    avatar style image methods

--- JSMessagesTableViewController/UIImage+JSMessagesView.h
+++ JSMessagesTableViewController/UIImage+JSMessagesView.h
@@ -30,6 +30,16 @@
 
 @interface UIImage (JSMessagesView)
 
+#pragma mark - Avatar styles
+- (UIImage *)circleImageWithSize:(CGFloat)size;
+- (UIImage *)squareImageWithSize:(CGFloat)size;
+
+- (UIImage *)imageAsCircle:(BOOL)clipToCircle
+               withDiamter:(CGFloat)diameter
+               borderColor:(UIColor *)borderColor
+               borderWidth:(CGFloat)borderWidth
+              shadowOffSet:(CGSize)shadowOffset;
+
 #pragma mark - Input bar
 + (UIImage *)inputBar;
 + (UIImage *)inputField;
--- JSMessagesTableViewController/UIImage+JSMessagesView.m
+++ JSMessagesTableViewController/UIImage+JSMessagesView.m
@@ -30,6 +30,81 @@
 
 @implementation UIImage (JSMessagesView)
 
+#pragma mark - Avatar styles
+- (UIImage *)circleImageWithSize:(CGFloat)size
+{
+    return [self imageAsCircle:YES
+                   withDiamter:size
+                   borderColor:[UIColor colorWithHue:0.0f saturation:0.0f brightness:0.8f alpha:1.0f]
+                   borderWidth:1.0f
+                  shadowOffSet:CGSizeMake(0.0f, 1.0f)];
+}
+
+- (UIImage *)squareImageWithSize:(CGFloat)size
+{
+    return [self imageAsCircle:NO
+                   withDiamter:size
+                   borderColor:[UIColor colorWithHue:0.0f saturation:0.0f brightness:0.8f alpha:1.0f]
+                   borderWidth:1.0f
+                  shadowOffSet:CGSizeMake(0.0f, 1.0f)];
+}
+
+- (UIImage *)imageAsCircle:(BOOL)clipToCircle
+               withDiamter:(CGFloat)diameter
+               borderColor:(UIColor *)borderColor
+               borderWidth:(CGFloat)borderWidth
+              shadowOffSet:(CGSize)shadowOffset
+{
+    // increase given size for border and shadow
+    CGFloat increase = diameter * 0.15f;
+    CGFloat newSize = diameter + increase;
+    
+    CGRect newRect = CGRectMake(0.0f,
+                                0.0f,
+                                newSize,
+                                newSize);
+    
+    // fit image inside border and shadow
+    CGRect imgRect = CGRectMake(increase,
+                                increase,
+                                newRect.size.width - (increase * 2.0f),
+                                newRect.size.height - (increase * 2.0f));
+    
+    UIGraphicsBeginImageContextWithOptions(newRect.size, NO, [UIScreen mainScreen].scale);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSaveGState(context);
+    
+    // draw shadow
+    if(!CGSizeEqualToSize(shadowOffset, CGSizeZero))
+        CGContextSetShadowWithColor(context,
+                                    CGSizeMake(shadowOffset.width, shadowOffset.height),
+                                    3.0f,
+                                    [UIColor colorWithWhite:0.0f alpha:0.45f].CGColor);
+    
+    // draw border
+    // as circle or square
+    CGPathRef borderPath = (clipToCircle) ? CGPathCreateWithEllipseInRect(imgRect, NULL) : CGPathCreateWithRect(imgRect, NULL);
+    
+    CGContextSetStrokeColorWithColor(context, borderColor.CGColor);
+    CGContextSetLineWidth(context, borderWidth);
+    CGContextAddPath(context, borderPath);
+    CGContextDrawPath(context, kCGPathFillStroke);
+    CGPathRelease(borderPath);
+    CGContextRestoreGState(context);
+    
+    // clip to circle
+    if(clipToCircle) {
+        UIBezierPath *imgPath = [UIBezierPath bezierPathWithOvalInRect:imgRect];
+        [imgPath addClip];
+    }
+    
+    [self drawInRect:imgRect];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
 #pragma mark - Input bar
 + (UIImage *)inputBar
 {
