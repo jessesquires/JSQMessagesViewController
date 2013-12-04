2806ef5120465816f6f72142d295a87de1a7a77d
Jesse Squires <jesse.d.squires@gmail.com>
2013-12-03 23:06:55 -0800

    fix avatar factory docs

--- JSMessagesViewController/Classes/Factories/JSAvatarImageFactory.h
+++ JSMessagesViewController/Classes/Factories/JSAvatarImageFactory.h
@@ -20,7 +20,7 @@
 extern CGFloat const kJSAvatarImageSize;
 
 /**
- *  `JSAvatarImageFactory` is a factory that provides a means for styling avatar images to be displayed in a `JSMessagesViewController`.
+ *  `JSAvatarImageFactory` is a factory that provides a means for styling avatar images to be displayed in a `JSBubbleMessageCell` of a `JSMessagesViewController`.
  */
 @interface JSAvatarImageFactory : NSObject
 
