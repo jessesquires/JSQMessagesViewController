e2c401bfbaa5182f8fef4003f9d77160e7218068
Jesse Squires <jesse.d.squires@gmail.com>
2013-12-03 22:47:18 -0800

    avatar factory docs. yay for not being lazy!

--- JSMessagesViewController/Classes/Factories/JSAvatarImageFactory.h
+++ JSMessagesViewController/Classes/Factories/JSAvatarImageFactory.h
@@ -14,13 +14,32 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *  A constant defining the size of an avatar image.
+ */
 extern CGFloat const kJSAvatarImageSize;
 
 @interface JSAvatarImageFactory : NSObject
 
+/**
+ *  Returns the image object associated with the specified filename. The image is cropped to a circle if the value of croppedToCircle is `YES`, otherwise the image is cropped to a square. The image has a flat, iOS 7 appearance.
+ *
+ *  @param filename        The name of the file. If this is the first time the image is being loaded, the method looks for an image with the specified name in the application’s main bundle.
+ *  @param croppedToCircle A boolean value indicating whether or not the image should be cropped as a circle or square. Pass `YES` to crop to a circle, and `NO` to crop to a square.
+ *
+ *  @return The image object for the specified file (cropped as specified), or `nil` if the method could not find the specified image.
+ */
 + (UIImage *)avatarImageNamed:(NSString *)filename
               croppedToCircle:(BOOL)croppedToCircle;
 
+/**
+ *  Returns the image object associated with the specified filename. The image is cropped to a circle if the value of croppedToCircle is `YES`, otherwise the image is cropped to a square. The image has a glossy, iOS 6 appearance.
+ *
+ *  @param filename        The name of the file. If this is the first time the image is being loaded, the method looks for an image with the specified name in the application’s main bundle.
+ *  @param croppedToCircle A boolean value indicating whether or not the image should be cropped as a circle or square. Pass `YES` to crop to a circle, and `NO` to crop to a square.
+ *
+ *  @return The image object for the specified file (cropped as specified), or `nil` if the method could not find the specified image.
+ */
 + (UIImage *)classicAvatarImageNamed:(NSString *)filename
                      croppedToCircle:(BOOL)croppedToCircle;
 
