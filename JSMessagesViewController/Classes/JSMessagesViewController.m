4a5dc2029eec2acc5176f9eca0aac54103760e0f
Tom Winzig <thomas@winzig.com>
2013-12-22 15:40:55 -0800

    Eliminating compiler warnings (some due to 64-bit).

--- JSMessagesViewController/Classes/JSMessagesViewController.m
+++ JSMessagesViewController/Classes/JSMessagesViewController.m
@@ -213,7 +213,7 @@
     BOOL hasAvatar = [self shouldHaveAvatarForRowAtIndexPath:indexPath];
 	BOOL hasSubtitle = [self shouldHaveSubtitleForRowAtIndexPath:indexPath];
     
-    NSString *CellIdentifier = [NSString stringWithFormat:@"MessageCell_%d_%d_%d_%d", type, hasTimestamp, hasAvatar, hasSubtitle];
+    NSString *CellIdentifier = [NSString stringWithFormat:@"MessageCell_%d_%d_%d_%d", (int)type, hasTimestamp, hasAvatar, hasSubtitle];
     JSBubbleMessageCell *cell = (JSBubbleMessageCell *)[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     
     if(!cell) {
