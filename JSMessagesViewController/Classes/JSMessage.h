a06935079d8845dc6eaccf7028d8e399dbc7bf19
Jesse Squires <jesse.d.squires@gmail.com>
2014-01-21 22:08:03 -0800

    add JSMessage object.

---
+++ JSMessagesViewController/Classes/JSMessage.h
@@ -0,0 +1,54 @@
+//
+//  Created by Jesse Squires
+//  http://www.hexedbits.com
+//
+//
+//  Documentation
+//  http://cocoadocs.org/docsets/JSMessagesViewController
+//
+//
+//  The MIT License
+//  Copyright (c) 2013 Jesse Squires
+//  http://opensource.org/licenses/MIT
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  A `JSMessage` object represents a single user message. It contains the message text, its sender, and the date that the message was sent.
+ */
+@interface JSMessage : NSObject <NSCoding, NSCopying>
+
+/**
+ *  The body text of the message. The default value is `nil`.
+ */
+@property (copy, nonatomic) NSString *text;
+
+/**
+ *  The name of user who sent the message. The default value is `nil`.
+ */
+@property (copy, nonatomic) NSString *sender;
+
+/**
+ *  The date that the message was sent. The default value is `nil`.
+ */
+@property (strong, nonatomic) NSDate *date;
+
+//@property (strong, nonatomic) NSURL *mediaURL;
+
+#pragma mark - Initialization
+
+/**
+ *  Initializes and returns a message object having the given text, sender, and date.
+ *
+ *  @param text   The body text of the message.
+ *  @param sender The name of the user who sent the message.
+ *  @param date   The date that the message was sent.
+ *
+ *  @return An initialized `JSMessage` object or `nil` if the object could not be successfully initialized.
+ */
+- (instancetype)initWithText:(NSString *)text
+                      sender:(NSString *)sender
+                        date:(NSDate *)date;
+
+@end
