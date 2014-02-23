8efa202e475f4b6b2e7b274059a6eb44076031f2
Jesse Squires <jesse.d.squires@gmail.com>
2014-02-23 13:08:26 -0800

    begin new collection view storyboard

--- JSMessagesDemo/JSAppDelegate.m
+++ JSMessagesDemo/JSAppDelegate.m
@@ -19,13 +19,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    JSDemoViewController *vc = [[JSDemoViewController alloc] init];
-	UINavigationController *nc = [[UINavigationController alloc] initWithRootViewController:vc];
-    
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-	self.window.rootViewController = nc;
-	[self.window makeKeyAndVisible];
-    
     return YES;
 }
 
