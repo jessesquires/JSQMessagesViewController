6fdb742e9cd6ff1f7d82efa69f89c6d1de921c17
Jesse Squires <jesse.d.squires@gmail.com>
2014-02-23 19:38:19 -0800

    stub out collection view cells

---
+++ JSQMessagesViewController/Cells/JSQMessagesCollectionViewCellIncoming.m
@@ -0,0 +1,19 @@
+//
+//  Created by Jesse Squires
+//  http://www.hexedbits.com
+//
+//
+//  Documentation
+//  http://cocoadocs.org/docsets/JSMessagesViewController
+//
+//
+//  The MIT License
+//  Copyright (c) 2014 Jesse Squires
+//  http://opensource.org/licenses/MIT
+//
+
+#import "JSQMessagesCollectionViewCellIncoming.h"
+
+@implementation JSQMessagesCollectionViewCellIncoming
+
+@end
