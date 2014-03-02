d755624bc58f7182340d82ada360295e057c7a69
Jesse Squires <jesse.d.squires@gmail.com>
2014-03-01 20:16:27 -0800

    hookup cell constraint outlets.

--- JSQMessagesViewController/Cells/JSQMessagesCollectionViewCell.h
+++ JSQMessagesViewController/Cells/JSQMessagesCollectionViewCell.h
@@ -22,6 +22,13 @@
 @property (weak, nonatomic) IBOutlet UIView *avatarContainerView;
 @property (weak, nonatomic) IBOutlet UILabel *cellBottomLabel;
 
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *cellTopLabelHeight;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bubbleTopLabelHeight;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *avatarContainerViewWidth;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *avatarContainerViewHeight;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *cellBottomLabelHeight;
+
 #pragma mark - Class methods
 
 + (UINib *)nib;
