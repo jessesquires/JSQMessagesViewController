51ed4773ffa1618781d3a5f9a0d05ff29422f2a4
Jesse Squires <jesse.d.squires@gmail.com>
2014-03-08 18:51:07 -0800

    update demo

--- JSQMessagesDemo/JSQTableViewController.m
+++ JSQMessagesDemo/JSQTableViewController.m
@@ -36,7 +36,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 1;
+    return 2;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -48,30 +48,39 @@
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     }
     
-    cell.textLabel.text = @"Push";
+    switch (indexPath.row) {
+        case 0:
+            cell.textLabel.text = @"Push via storyboard";
+            break;
+        case 1:
+            cell.textLabel.text = @"Push programmatically";
+            break;
+    }
+    
     return cell;
 }
 
-#pragma mark - Table view delegate
-
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
-    return @"Select Presentation";
+    return @"Choose Presentation";
 }
+
+- (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section
+{
+    return @"Copyright © 2014\nJesse Squires\nMIT License";
+}
+#pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-//    UIStoryboard *sb = [JSQMessagesViewController messagesStoryboard];
-//    JSQMessagesViewController *vc = [sb instantiateInitialViewController];
-    
-//    JSQDemoViewController *vc = (JSQDemoViewController *)[JSQMessagesViewController messagesViewController];
-    
-    JSQDemoViewController *vc = [JSQDemoViewController messagesViewController];
-
-    NSLog(@"CLASS = %@", [vc class]);
-    
-//    JSQDemoViewController *vc = [self.storyboard instantiateViewControllerWithIdentifier:@"DemoVC"];
-    [self.navigationController pushViewController:vc animated:YES];
+    switch (indexPath.row) {
+        case 0:
+            [self performSegueWithIdentifier:@"seguePushDemoVC" sender:self];
+            break;
+        case 1:
+            [self.navigationController pushViewController:[JSQDemoViewController messagesViewController] animated:YES];
+            break;
+    }
 }
 
 @end
