42ae44b7515eac5ad05c1695ba4d31a9d93e0952
Jesse Squires <jesse.d.squires@gmail.com>
2014-03-22 11:32:47 -0700

    supplementary view

---
+++ JSQMessagesViewController/Cells/JSQMessagesCollectionSupplementaryView.h
@@ -0,0 +1,36 @@
+//
+//  Created by Jesse Squires
+//  http://www.hexedbits.com
+//
+//
+//  Documentation
+//  http://cocoadocs.org/docsets/JSMessagesViewController
+//
+//
+//  The MIT License
+//  Copyright (c) 2014 Jesse Squires
+//  http://opensource.org/licenses/MIT
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JSQMessagesCollectionViewCellLabel.h"
+
+
+FOUNDATION_EXPORT const CGFloat kJSQMessagesCollectionSupplementaryViewHeight;
+
+FOUNDATION_EXPORT NSString * const kJSQMessagesCollectionSupplementaryViewCellHeader;
+FOUNDATION_EXPORT NSString * const kJSQMessagesCollectionSupplementaryViewCellFooter;
+
+
+@interface JSQMessagesCollectionSupplementaryView : UICollectionReusableView
+
+@property (weak, nonatomic, readonly) JSQMessagesCollectionViewCellLabel *label;
+
+#pragma mark - Class methods
+
++ (UINib *)nib;
+
++ (NSString *)supplementaryViewReuseIdentifier;
+
+@end
