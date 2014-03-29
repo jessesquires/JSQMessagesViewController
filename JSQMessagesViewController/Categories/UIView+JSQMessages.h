2ce8e413a470ccc2a8380d727ed06c9db732fc96
Jesse Squires <jesse.d.squires@gmail.com>
2014-03-28 22:32:34 -0700

    view category docs

--- JSQMessagesViewController/Categories/UIView+JSQMessages.h
+++ JSQMessagesViewController/Categories/UIView+JSQMessages.h
@@ -16,8 +16,19 @@
 
 @interface UIView (JSQMessages)
 
+/**
+ *  Pins the subview of the receiver to the edge of its frame, as specified by the given attribute, by adding a layout constraint.
+ *
+ *  @param subview   The subview to which the receiver will be pinned.
+ *  @param attribute The layout constraint attribute specifying one of `NSLayoutAttributeBottom`, `NSLayoutAttributeTop`, `NSLayoutAttributeLeading`, `NSLayoutAttributeTrailing`.
+ */
 - (void)jsq_pinSubview:(UIView *)subview toEdge:(NSLayoutAttribute)attribute;
 
+/**
+ *  Pins all edges of the specified subview to the receiver.
+ *
+ *  @param subview The subview to which the receiver will be pinned.
+ */
 - (void)jsq_pinAllEdgesOfSubview:(UIView *)subview;
 
 @end
