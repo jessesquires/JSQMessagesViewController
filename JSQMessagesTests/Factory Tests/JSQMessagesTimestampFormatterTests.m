ad3c9d219da50b802c2eb030914fe96512d46393
Jesse Squires <jesse.d.squires@gmail.com>
2014-04-24 22:38:39 -0700

    begin formatter tests

---
+++ JSQMessagesTests/Factory Tests/JSQMessagesTimestampFormatterTests.m
@@ -0,0 +1,28 @@
+//
+//  Created by Jesse Squires
+//  http://www.hexedbits.com
+//
+//
+//  The MIT License
+//  Copyright (c) 2014 Jesse Squires
+//  http://opensource.org/licenses/MIT
+//
+
+#import <XCTest/XCTest.h>
+
+#import "JSQMessagesTimestampFormatter.h"
+
+
+@interface JSQMessagesTimestampFormatterTests : XCTestCase
+@end
+
+
+@implementation JSQMessagesTimestampFormatterTests
+
+- (void)testTimestampFormatterInit
+{
+    JSQMessagesTimestampFormatter *formatter = [JSQMessagesTimestampFormatter sharedFormatter];
+    XCTAssertNotNil(formatter, @"Formatter should not be nil");
+}
+
+@end
