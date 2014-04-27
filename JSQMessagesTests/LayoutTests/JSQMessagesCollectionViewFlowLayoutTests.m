8db1e1d959838db9fd0fcfc24a08d1aaf6d51036
Jesse Squires <jesse.d.squires@gmail.com>
2014-04-27 12:10:41 -0700

    stub out new tests

---
+++ JSQMessagesTests/LayoutTests/JSQMessagesCollectionViewFlowLayoutTests.m
@@ -0,0 +1,31 @@
+//
+//  Created by Jesse Squires
+//  http://www.hexedbits.com
+//
+//
+//  MIT License
+//  Copyright (c) 2014 Jesse Squires
+//  http://opensource.org/licenses/MIT
+//
+
+#import <XCTest/XCTest.h>
+
+
+@interface JSQMessagesCollectionViewFlowLayoutTests : XCTestCase
+
+@end
+
+
+@implementation JSQMessagesCollectionViewFlowLayoutTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+@end
---
+++ JSQMessagesTests/ModelTests/JSQMessageTests.m
@@ -0,0 +1,31 @@
+//
+//  Created by Jesse Squires
+//  http://www.hexedbits.com
+//
+//
+//  MIT License
+//  Copyright (c) 2014 Jesse Squires
+//  http://opensource.org/licenses/MIT
+//
+
+#import <XCTest/XCTest.h>
+
+
+@interface JSQMessageTests : XCTestCase
+
+@end
+
+
+@implementation JSQMessageTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+@end
