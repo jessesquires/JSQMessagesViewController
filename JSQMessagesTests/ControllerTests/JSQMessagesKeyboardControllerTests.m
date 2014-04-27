fb33e14894f3a6d96ea4b4a983ef3c07bb3cc425
Jesse Squires <jesse.d.squires@gmail.com>
2014-04-27 12:04:56 -0700

    begin keyboard controller tests

---
+++ JSQMessagesTests/ControllerTests/JSQMessagesKeyboardControllerTests.m
@@ -0,0 +1,36 @@
+//
+//  Created by Jesse Squires
+//  http://www.hexedbits.com
+//
+//
+//  MIT License
+//  Copyright (c) 2014 Jesse Squires
+//  http://opensource.org/licenses/MIT
+//
+
+#import <XCTest/XCTest.h>
+
+#import "JSQMessagesKeyboardController.h"
+
+
+@interface JSQMessagesKeyboardControllerTests : XCTestCase
+@end
+
+
+@implementation JSQMessagesKeyboardControllerTests
+
+- (void)testKeyboardControllerInit
+{
+    JSQMessagesKeyboardController *keyboardController = [[JSQMessagesKeyboardController alloc] initWithTextView:[UITextView new]
+                                                                                                    contextView:[UIView new]
+                                                                                           panGestureRecognizer:[UIPanGestureRecognizer new]
+                                                                                                       delegate:nil];
+    XCTAssertNotNil(keyboardController, @"Keyboard controller should not be nil");
+    
+    XCTAssertThrows([[JSQMessagesKeyboardController alloc] initWithTextView:nil
+                                                                contextView:nil
+                                                       panGestureRecognizer:nil
+                                                                   delegate:nil], @"Invalid init should throw");
+}
+
+@end
