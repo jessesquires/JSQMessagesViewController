7a4fba830ebb052685513eb4064a9433489e57cf
Jesse Squires <jesse.d.squires@gmail.com>
2014-04-27 22:58:11 -0700

    begin collection view cell tests

---
+++ JSQMessagesTests/ViewTests/JSQMessagesCollectionViewCellTests.m
@@ -0,0 +1,48 @@
+//
+//  Created by Jesse Squires
+//  http://www.hexedbits.com
+//
+//
+//  MIT License
+//  Copyright (c) 2014 Jesse Squires
+//  http://opensource.org/licenses/MIT
+//
+
+#import <XCTest/XCTest.h>
+
+#import "JSQMessagesCollectionViewCellIncoming.h"
+#import "JSQMessagesCollectionViewCellOutgoing.h"
+
+
+@interface JSQMessagesCollectionViewCellTests : XCTestCase
+@end
+
+
+@implementation JSQMessagesCollectionViewCellTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testMessagesCollectionViewCellInit
+{
+    UINib *incomingCell = [JSQMessagesCollectionViewCellIncoming nib];
+    XCTAssertNotNil(incomingCell, @"Nib should not be nil");
+    
+    NSString *incomingCellId = [JSQMessagesCollectionViewCellIncoming cellReuseIdentifier];
+    XCTAssertNotNil(incomingCellId, @"Cell identifier should not be nil");
+    
+    UINib *outgoingCell = [JSQMessagesCollectionViewCellOutgoing nib];
+    XCTAssertNotNil(outgoingCell, @"Nib should not be nil");
+    
+    NSString *outgoingCellId = [JSQMessagesCollectionViewCellOutgoing cellReuseIdentifier];
+    XCTAssertNotNil(outgoingCellId, @"Cell identifier should not be nil");
+}
+
+@end
