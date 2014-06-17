c57e62e35f7505ddd6a418ffa887f244aacf7ece
Pierluigi Cifani <pcifani@voalte.com>
2014-06-17 19:09:14 +0200

    Added async media support

--- JSQMessagesViewController/Views/JSQMessagesMediaHandler.m
+++ JSQMessagesViewController/Views/JSQMessagesMediaHandler.m
@@ -8,10 +8,12 @@
 
 #import "JSQMessagesMediaHandler.h"
 #import "JSQMessagesCollectionViewCell.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
 @interface JSQMessagesMediaHandler ()
 
 @property (nonatomic, weak) JSQMessagesCollectionViewCell *cell;
+@property (nonatomic, weak) UIActivityIndicatorView *activityIndicator;
 
 @end
 
@@ -35,22 +37,34 @@
 - (void) setMediaFromImage:(UIImage *)image;
 {
     self.cell.mediaImageView.image = image;
-    [self maskImageViewWithBallon];
+    [self maskImageViewWithBubble];
 }
 
 - (void) setMediaFromURL:(NSURL *)imageURL;
 {
-
+    [self addActitityIndicator];
+    
+    __weak __typeof(self) weakSelf = self;
+    
+    [self.cell.mediaImageView setImageWithURL:imageURL
+                                    completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType) {
+                                        
+                                        __typeof(self) strongSelf = weakSelf;
+                                        [strongSelf maskImageViewWithBubble];
+                                        [strongSelf removeActitityIndicator];
+                                    }];
 }
 
 - (void) cellWillBeReused;
 {
     self.cell.mediaImageView.image = nil;
+    [self.cell.mediaImageView cancelCurrentImageLoad];
+    [self removeActitityIndicator];
 }
 
 #pragma mark Private
 
-- (void) maskImageViewWithBallon
+- (void) maskImageViewWithBubble
 {
     /**
      *  Snippet from https://github.com/SocialObjects-Software/SOMessaging
@@ -59,6 +73,43 @@
     layer.frame = (CGRect){{0,0},self.cell.messageBubbleImageView.layer.frame.size};
     self.cell.mediaImageView.layer.mask = layer;
     [self.cell.mediaImageView setNeedsDisplay];
+}
+
+- (void) addActitityIndicator
+{
+    UIActivityIndicatorView *activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+    [activityIndicator setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    [self.cell.messageBubbleImageView addSubview:activityIndicator];
+    
+    /**
+     *  Center in superview
+     */
+    UIView *superview = self.cell.messageBubbleImageView;
+    NSDictionary *variables = NSDictionaryOfVariableBindings(activityIndicator, superview);
+    NSArray *constraints =
+    [NSLayoutConstraint constraintsWithVisualFormat:@"V:[superview]-(<=1)-[activityIndicator]"
+                                            options: NSLayoutFormatAlignAllCenterX
+                                            metrics:nil
+                                              views:variables];
+    [self.cell.contentView addConstraints:constraints];
+    
+    constraints =
+    [NSLayoutConstraint constraintsWithVisualFormat:@"H:[superview]-(<=1)-[activityIndicator]"
+                                            options: NSLayoutFormatAlignAllCenterY
+                                            metrics:nil
+                                              views:variables];
+    [self.cell.contentView addConstraints:constraints];
+    
+    [activityIndicator startAnimating];
+    
+    self.activityIndicator = activityIndicator;
+}
+
+- (void) removeActitityIndicator
+{
+    [self.activityIndicator removeFromSuperview];
+    self.activityIndicator = nil;
 }
 
 @end
