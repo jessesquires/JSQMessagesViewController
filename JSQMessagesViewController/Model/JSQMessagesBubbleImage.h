1787b82224d26ad441464ea0e6a2fcbeaac3efe9
Jesse Squires <jesse.d.squires@gmail.com>
2014-09-01 21:59:50 -0700

    add new JSQMessagesBubbleImage object. concrete JSQMessageBubbleImageDataSource. #319

---
+++ JSQMessagesViewController/Model/JSQMessagesBubbleImage.h
@@ -0,0 +1,30 @@
+//
+//  Created by Jesse Squires
+//  http://www.hexedbits.com
+//
+//
+//  Documentation
+//  http://cocoadocs.org/docsets/JSQMessagesViewController
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQMessagesViewController
+//
+//
+//  License
+//  Copyright (c) 2014 Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+#import <Foundation/Foundation.h>
+#import "JSQMessageBubbleImageDataSource.h"
+
+@interface JSQMessagesBubbleImage : NSObject <JSQMessageBubbleImageDataSource, NSCopying>
+
+@property (strong, nonatomic, readonly) UIImage *messageBubbleImage;
+
+@property (strong, nonatomic, readonly) UIImage *messageBubbleHighlightedImage;
+
+- (instancetype)initWithMessageBubbleImage:(UIImage *)image highlightedImage:(UIImage *)highlightedImage;
+
+@end
