a434273f039f6506e36b189ad2cd82155c81df83
Jesse Squires <jesse.d.squires@gmail.com>
2014-09-06 16:00:52 -0700

    add documentation for JSQMessageBubbleImageDataSource and JSQMessagesBubbleImage. ref #319

--- JSQMessagesViewController/Model/JSQMessageBubbleImageDataSource.h
+++ JSQMessagesViewController/Model/JSQMessageBubbleImageDataSource.h
@@ -19,14 +19,34 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-// TODO: documentation
-
+/**
+ *  The `JSQMessageBubbleImageDataSource` protocol defines the common interface through which
+ *  a `JSQMessagesViewController` and `JSQMessagesCollectionView` interact with 
+ *  message bubble image model objects.
+ *
+ *  It declares the required and optional methods that a class must implement so that instances
+ *  of that class can be display properly within a `JSQMessagesCollectionViewCell`.
+ *
+ *  A concrete class that conforms to this protocol is provided in the library. See `JSQMessagesBubbleImage`.
+ *
+ *  @see JSQMessagesBubbleImage.
+ */
 @protocol JSQMessageBubbleImageDataSource <NSObject>
 
 @required
 
+/**
+ *  @return The message bubble image for a regular display state.
+ *
+ *  @warning You must not return `nil` from this method.
+ */
 - (UIImage *)messageBubbleImage;
 
+/**
+ *  @return The message bubble image for a highlighted display state.
+ *
+ *  @warning You must not return `nil` from this method.
+ */
 - (UIImage *)messageBubbleHighlightedImage;
 
 @end
--- JSQMessagesViewController/Model/JSQMessagesBubbleImage.h
+++ JSQMessagesViewController/Model/JSQMessagesBubbleImage.h
@@ -20,15 +20,35 @@
 #import <UIKit/UIKit.h>
 #import "JSQMessageBubbleImageDataSource.h"
 
-// TODO: documentation
-
-
+/**
+ *  A `JSQMessagesBubbleImage` model object represents a message bubble image, and is immutable. 
+ *  This is a concrete class that implements the `JSQMessageBubbleImageDataSource` protocol.
+ *  It contains a regular message bubble image and a highlighted message bubble image.
+ *
+ *  @see JSQMessagesBubbleImageFactory.
+ */
 @interface JSQMessagesBubbleImage : NSObject <JSQMessageBubbleImageDataSource, NSCopying>
 
+/**
+ *  Returns the message bubble image for a regular display state.
+ */
 @property (strong, nonatomic, readonly) UIImage *messageBubbleImage;
 
+/**
+ *  Returns the message bubble image for a highlighted display state.
+ */
 @property (strong, nonatomic, readonly) UIImage *messageBubbleHighlightedImage;
 
+/**
+ *  Initializes and returns a message bubble image object having the specified regular image and highlighted image.
+ *
+ *  @param image            The regular message bubble image. This value must not be `nil`.
+ *  @param highlightedImage The highlighted message bubble image. This value must not be `nil`.
+ *
+ *  @return An initialized `JSQMessagesBubbleImage` object if successful, `nil` otherwise.
+ *
+ *  @see JSQMessagesBubbleImageFactory.
+ */
 - (instancetype)initWithMessageBubbleImage:(UIImage *)image highlightedImage:(UIImage *)highlightedImage;
 
 @end
