0c404b528f2c5036782eb5ac36882b0df5620947
Jesse Squires <jesse.d.squires@gmail.com>
2014-09-16 20:34:59 -0700

    prevent messageBubbleLeftRightMargin from being negative. fix #486

--- JSQMessagesViewController/Layout/JSQMessagesCollectionViewFlowLayout.m
+++ JSQMessagesViewController/Layout/JSQMessagesCollectionViewFlowLayout.m
@@ -160,6 +160,7 @@ const CGFloat kJSQMessagesCollectionViewCellLabelHeightDefault = 20.0f;
 
 - (void)setMessageBubbleLeftRightMargin:(CGFloat)messageBubbleLeftRightMargin
 {
+    NSParameterAssert(messageBubbleLeftRightMargin >= 0.0f);
     _messageBubbleLeftRightMargin = messageBubbleLeftRightMargin;
     [self invalidateLayoutWithContext:[JSQMessagesCollectionViewFlowLayoutInvalidationContext context]];
 }
@@ -402,7 +403,7 @@ const CGFloat kJSQMessagesCollectionViewCellLabelHeightDefault = 20.0f;
     CGFloat textPadding = [self jsq_messageBubbleTextContainerInsetsTotal];
     CGFloat messageBubblePadding = remainingItemWidthForBubble - messageBubbleSize.width - textPadding;
     
-    layoutAttributes.messageBubbleLeftRightMargin = messageBubblePadding;
+    layoutAttributes.messageBubbleLeftRightMargin = MAX(messageBubblePadding, 0.0f);
     
     layoutAttributes.textViewFrameInsets = self.messageBubbleTextViewFrameInsets;
     
