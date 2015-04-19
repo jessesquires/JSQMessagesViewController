cd974aa01599c90d59776f6f3845c1a671bec793
Jesse Squires <jesse.d.squires@gmail.com>
2015-04-19 11:38:37 -0700

    suppress warning for now. fix in next release, see #920

--- JSQMessagesViewController/Layout/JSQMessagesCollectionViewFlowLayout.h
+++ JSQMessagesViewController/Layout/JSQMessagesCollectionViewFlowLayout.h
@@ -56,7 +56,12 @@ FOUNDATION_EXPORT const CGFloat kJSQMessagesCollectionViewAvatarSizeDefault;
 /**
  *  The collection view object currently using this layout object.
  */
+
+// TODO: fix, rename "messagesCollectionView", see #920
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wincompatible-property-type"
 @property (readonly, nonatomic) JSQMessagesCollectionView *collectionView;
+#pragma clang diagnostic pop
 
 /**
  *  Specifies whether or not the layout should enable spring behavior dynamics for its items using `UIDynamics`.
