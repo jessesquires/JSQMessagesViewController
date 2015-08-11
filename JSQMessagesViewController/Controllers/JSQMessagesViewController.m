22fa1610d2c4a04b9faac683c9ef516485d15e65
Pritesh Shah <pritesh.shah@magnet.com>
2015-08-10 20:59:45 -0700

    Fixed a warning

--- JSQMessagesViewController/Controllers/JSQMessagesViewController.m
+++ JSQMessagesViewController/Controllers/JSQMessagesViewController.m
@@ -276,7 +276,7 @@ static void * kJSQMessagesKeyValueObservingContext = &kJSQMessagesKeyValueObserv
     return YES;
 }
 
-- (UIInterfaceOrientationMask)supportedInterfaceOrientations
+- (NSUInteger)supportedInterfaceOrientations
 {
     if ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPhone) {
         return UIInterfaceOrientationMaskAllButUpsideDown;
