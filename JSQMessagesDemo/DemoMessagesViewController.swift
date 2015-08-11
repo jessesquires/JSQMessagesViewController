e05a0160a62c1cec2efb3b150367ea842bf47e38
Raphaël Bellec <contact@bellec.in>
2015-08-11 23:27:01 +0200

    Start to covert DemoMessagesViewController. Add Outlets

---
+++ JSQMessagesDemo/DemoMessagesViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DemoMessagesViewController.swift
+//  JSQMessages
+//
+//  Created by Raphaël Bellec on 11/08/2015.
+//  Copyright (c) 2015 Hexed Bits. All rights reserved.
+//
+
+import Foundation
+
+class DemoMessagesViewController : UITableViewController {
+    
+    @IBOutlet var extraMessagesSwitch   : UISwitch!
+    @IBOutlet var longMessageSwitch     : UISwitch!
+    @IBOutlet var emptySwitch           : UISwitch!
+    @IBOutlet var incomingAvatarsSwitch : UISwitch!
+    @IBOutlet var outgoingAvatarsSwitch : UISwitch!
+    @IBOutlet var springySwitch         : UISwitch!
+
+        
+    
+    
+    
+}
+
+
