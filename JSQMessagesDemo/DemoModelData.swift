cfa42805d798681c3359d5eefc9168493a6a8371
Raphaël Bellec <contact@bellec.in>
2015-08-11 16:00:04 +0200

    correct NSDate.distantPast call. this call need a downcast to NSDate in swift.

--- JSQMessagesDemo/DemoModelData.swift
+++ JSQMessagesDemo/DemoModelData.swift
@@ -61,23 +61,18 @@ class DemoModelData : NSObject{
         *  If you are not using avatars, ignore this.
         */
         
-//        + (JSQMessagesAvatarImage *)avatarImageWithUserInitials:(NSString *)userInitials
-//        backgroundColor:(UIColor *)backgroundColor
-//        textColor:(UIColor *)textColor
-//        font:(UIFont *)font
-//        diameter:(NSUInteger)diameter;
         
-        let jsqImage : JSQMessagesAvatarImage = JSQMessagesAvatarImageFactory.avatarImageWithUserInitials( "JSQ",
+        let jsqImage : JSQMessagesAvatarImage    = JSQMessagesAvatarImageFactory.avatarImageWithUserInitials( "JSQ",
                                                                                     backgroundColor: UIColor(white: 0.85, alpha:1.0),
                                                                                           textColor: UIColor(white: 0.60, alpha:1.0),
                                                                                                font: UIFont.systemFontOfSize(14.0),
                                                                                            diameter: messagesCollectionViewAvatarSizeDefault
                                                                                 )
         
-        let cookImage : JSQMessagesAvatarImage = JSQMessagesAvatarImageFactory.avatarImageWithImage( UIImage(named: "demo_avatar_cook") , diameter:messagesCollectionViewAvatarSizeDefault )
-        let jobsImage : JSQMessagesAvatarImage = JSQMessagesAvatarImageFactory.avatarImageWithImage( UIImage(named: "demo_avatar_jobs") , diameter:messagesCollectionViewAvatarSizeDefault )
-        let wozImage  : JSQMessagesAvatarImage = JSQMessagesAvatarImageFactory.avatarImageWithImage( UIImage(named: "demo_avatar_woz" ) , diameter:messagesCollectionViewAvatarSizeDefault )
-//
+        let cookImage : JSQMessagesAvatarImage   = JSQMessagesAvatarImageFactory.avatarImageWithImage( UIImage(named: "demo_avatar_cook") , diameter:messagesCollectionViewAvatarSizeDefault )
+        let jobsImage : JSQMessagesAvatarImage   = JSQMessagesAvatarImageFactory.avatarImageWithImage( UIImage(named: "demo_avatar_jobs") , diameter:messagesCollectionViewAvatarSizeDefault )
+        let wozImage  : JSQMessagesAvatarImage   = JSQMessagesAvatarImageFactory.avatarImageWithImage( UIImage(named: "demo_avatar_woz" ) , diameter:messagesCollectionViewAvatarSizeDefault )
+
         
         self.avatars = [ kJSQDemoAvatarIdSquires : jsqImage,
                             kJSQDemoAvatarIdCook : cookImage,
@@ -90,10 +85,6 @@ class DemoModelData : NSObject{
                              kJSQDemoAvatarIdWoz : kJSQDemoAvatarDisplayNameWoz,
                          kJSQDemoAvatarIdSquires : kJSQDemoAvatarDisplayNameSquires ]
         
-
-        
-        
-        
     }
     
     
@@ -105,7 +96,7 @@ class DemoModelData : NSObject{
         *  Be sure to create your bubble images one time and reuse them for good performance.
         *
         */
-        let bubbleFactory = JSQMessagesBubbleImageFactory()
+        let bubbleFactory            = JSQMessagesBubbleImageFactory()
         
         self.outgoingBubbleImageData = bubbleFactory.outgoingMessagesBubbleImageWithColor( UIColor.jsq_messageBubbleLightGrayColor() )
         self.incomingBubbleImageData = bubbleFactory.incomingMessagesBubbleImageWithColor( UIColor.jsq_messageBubbleGreenColor()     )
@@ -117,20 +108,20 @@ class DemoModelData : NSObject{
         
         // Fake text messages
         messages = [
-            JSQMessage(senderId: kJSQDemoAvatarIdSquires, senderDisplayName:kJSQDemoAvatarDisplayNameSquires, date: NSDate.distantPast, text:"Welcome to JSQMessages: A messaging UI framework for iOS."),
-            JSQMessage(senderId: kJSQDemoAvatarIdWoz    , senderDisplayName:kJSQDemoAvatarDisplayNameWoz    , date: NSDate.distantPast, text:"It is simple, elegant, and easy to use. There are super sweet default settings, but you can customize like crazy."),
-            JSQMessage(senderId: kJSQDemoAvatarIdSquires, senderDisplayName:kJSQDemoAvatarDisplayNameSquires, date: NSDate.distantPast, text:"It even has data detectors. You can call me tonight. My cell number is 123-456-7890. My website is www.hexedbits.com."),
-            JSQMessage(senderId: kJSQDemoAvatarIdJobs   , senderDisplayName:kJSQDemoAvatarDisplayNameJobs   , date: NSDate.distantPast, text:"JSQMessagesViewController is nearly an exact replica of the iOS Messages App. And perhaps, better."),
-            JSQMessage(senderId: kJSQDemoAvatarIdCook   , senderDisplayName:kJSQDemoAvatarDisplayNameCook   , date: NSDate.distantPast, text:"It is unit-tested, free, open-source, and documented."),
-            JSQMessage(senderId: kJSQDemoAvatarIdSquires, senderDisplayName:kJSQDemoAvatarDisplayNameSquires, date: NSDate.distantPast, text:"Now with media messages!"),
+            JSQMessage(senderId: kJSQDemoAvatarIdSquires, senderDisplayName:kJSQDemoAvatarDisplayNameSquires, date: NSDate.distantPast() as! NSDate, text:"Welcome to JSQMessages: A messaging UI framework for iOS."),
+            JSQMessage(senderId: kJSQDemoAvatarIdWoz    , senderDisplayName:kJSQDemoAvatarDisplayNameWoz    , date: NSDate.distantPast() as! NSDate, text:"It is simple, elegant, and easy to use. There are super sweet default settings, but you can customize like crazy."),
+            JSQMessage(senderId: kJSQDemoAvatarIdSquires, senderDisplayName:kJSQDemoAvatarDisplayNameSquires, date: NSDate.distantPast() as! NSDate, text:"It even has data detectors. You can call me tonight. My cell number is 123-456-7890. My website is www.hexedbits.com."),
+            JSQMessage(senderId: kJSQDemoAvatarIdJobs   , senderDisplayName:kJSQDemoAvatarDisplayNameJobs   , date: NSDate.distantPast() as! NSDate, text:"JSQMessagesViewController is nearly an exact replica of the iOS Messages App. And perhaps, better."),
+            JSQMessage(senderId: kJSQDemoAvatarIdCook   , senderDisplayName:kJSQDemoAvatarDisplayNameCook   , date: NSDate.distantPast() as! NSDate, text:"It is unit-tested, free, open-source, and documented."),
+            JSQMessage(senderId: kJSQDemoAvatarIdSquires, senderDisplayName:kJSQDemoAvatarDisplayNameSquires, date: NSDate.distantPast() as! NSDate, text:"Now with media messages!"),
             
         ]
         
-        self.addPhotoMediaMessage
+        self.addPhotoMediaMessage()
         
         
         // Setting to load extra messages for testing/demo
-        if (NSUserDefaults.extraMessagesSetting) {
+        if (NSUserDefaults.extraMessagesSetting()) {
             // take the first 4 messages and add them to the end of message array
             messages += messages[0...3]
         }
@@ -139,7 +130,7 @@ class DemoModelData : NSObject{
         
         // Setting to load REALLY long message for testing/demo
         // You should see "END" twice
-        if (NSUserDefaults.longMessageSetting) {
+        if (NSUserDefaults.longMessageSetting()) {
             let long_text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? END Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? END"
          
             let reallyLongMessage = JSQMessage(senderId: kJSQDemoAvatarIdSquires, senderDisplayName:kJSQDemoAvatarDisplayNameSquires, date: NSDate.distantPast, text:long_text)
