2e5e922c229e6f9d24f5392ce3af132e0b70ea23
Raphaël Bellec <contact@bellec.in>
2015-08-13 15:30:34 +0200

    Work in progress in DemoMessagesViewController

--- JSQMessagesDemo/DemoMessagesViewController.swift
+++ JSQMessagesDemo/DemoMessagesViewController.swift
@@ -320,11 +320,199 @@ class DemoMessagesViewController : JSQMessagesViewController, UIActionSheetDeleg
         sheet.showFromToolbar(self.inputToolbar)
     }
 
+    func actionSheet(actionSheet: UIActionSheet, didDismissWithButtonIndex buttonIndex: Int) {
+        
+        if (buttonIndex == actionSheet.cancelButtonIndex) {
+            return;
+        }
+        
+        switch (buttonIndex) {
+        case 0:
+            self.demoData.addPhotoMediaMessage()
+            
+        case 1:
+            weak var weakView : UICollectionView? = self.collectionView
+            self.demoData.addLocationMediaMessageCompletion( { weakView?.reloadData() } )
+            
+        case 2:
+            self.demoData.addVideoMediaMessage()
+            
+        default:
+            // Swift require switch to be exhaustive. 
+            NSLog("%s error: unrecognized button index, line :%s", __FUNCTION__, __LINE__)
+
+        }
+        
+        JSQSystemSoundPlayer.jsq_playMessageSentSound()
+        
+        self.finishSendingMessageAnimated(true)
+    }
     
     
     
+    // ----------------------------------------------------------------------
+    // MARK: - JSQMessages CollectionView DataSource
+    // ----------------------------------------------------------------------
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+        return self.demoData.messages[indexPath.item]
+    }
+
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
+        
+    /**
+    *  You may return nil here if you do not want bubbles.
+    *  In this case, you should set the background color of your collection view cell's textView.
+    *
+    *  Otherwise, return your previously created bubble image data objects.
+    */
+    
+        let  message : JSQMessage! = self.demoData.messages[indexPath.item];
+        if   message.senderId == self.senderId {
+            return self.demoData.outgoingBubbleImageData
+        } else {
+            return self.demoData.incomingBubbleImageData
+        }
+    }
+    
+    override  func collectionView(collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource? {
+        
+        /**
+        *  Return `nil` here if you do not want avatars.
+        *  If you do return `nil`, be sure to do the following in `viewDidLoad`:
+        *
+        *  self.collectionView.collectionViewLayout.incomingAvatarViewSize = CGSizeZero;
+        *  self.collectionView.collectionViewLayout.outgoingAvatarViewSize = CGSizeZero;
+        *
+        *  It is possible to have only outgoing avatars or only incoming avatars, too.
+        */
+        
+        /**
+        *  Return your previously created avatar image data objects.
+        *
+        *  Note: these the avatars will be sized according to these values:
+        *
+        *  self.collectionView.collectionViewLayout.incomingAvatarViewSize
+        *  self.collectionView.collectionViewLayout.outgoingAvatarViewSize
+        *
+        *  Override the defaults in `viewDidLoad`
+        */
+        let message : JSQMessage = self.demoData.messages[indexPath.item]
+        
+        if message.senderId == self.senderId {
+            if !NSUserDefaults.outgoingAvatarSetting() {
+                return nil
+            }
+        } else {
+            if !NSUserDefaults.incomingAvatarSetting() {
+                return nil;
+            }
+        }
+        
+        
+        return self.demoData.avatars[message.senderId]
+    }
+
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, attributedTextForCellTopLabelAtIndexPath indexPath: NSIndexPath!) -> NSAttributedString? {
+        
+        /**
+        *  This logic should be consistent with what you return from `heightForCellTopLabelAtIndexPath:`
+        *  The other label text delegate methods should follow a similar pattern.
+        *
+        *  Show a timestamp for every 3rd message
+        */
+        if (indexPath.item % 3 == 0) {
+            let  message : JSQMessage = self.demoData.messages[indexPath.item]
+            return JSQMessagesTimestampFormatter.sharedFormatter().attributedTimestampForDate(message.date)
+        }
+        
+        return nil
+    }
     
     
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, attributedTextForMessageBubbleTopLabelAtIndexPath indexPath: NSIndexPath!) -> NSAttributedString? {
+        
+        let  message : JSQMessage = self.demoData.messages[indexPath.item]
+        
+        /**
+        *  iOS7-style sender name labels
+        */
+        if  message.senderId == self.senderId  {
+            return nil
+        }
+        
+        if  indexPath.item - 1 > 0 {
+            let  previousMessage : JSQMessage = self.demoData.messages[indexPath.item - 1]
+            if   previousMessage.senderId == message.senderId  {
+                return nil;
+            }
+        }
+        
+        /**
+        *  Don't specify attributes to use the defaults.
+        */
+        return NSAttributedString(string: message.senderDisplayName)
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, attributedTextForCellBottomLabelAtIndexPath indexPath: NSIndexPath!) -> NSAttributedString? {
+        return nil
+    }
+    
+    
+    // ----------------------------------------------------------------------
+    // MARK: - UICollectionView DataSource
+    // ----------------------------------------------------------------------
+    
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.demoData.messages.count
+    }
+    
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        /**
+        *  Override point for customizing cells
+        */
+        let cell : JSQMessagesCollectionViewCell = super.collectionView(collectionView, cellForItemAtIndexPath:indexPath) as! JSQMessagesCollectionViewCell
+        
+        /**
+        *  Configure almost *anything* on the cell
+        *
+        *  Text colors, label text, label colors, etc.
+        *
+        *
+        *  DO NOT set `cell.textView.font` !
+        *  Instead, you need to set `self.collectionView.collectionViewLayout.messageBubbleFont` to the font you want in `viewDidLoad`
+        *
+        *
+        *  DO NOT manipulate cell layout information!
+        *  Instead, override the properties you want on `self.collectionView.collectionViewLayout` from `viewDidLoad`
+        */
+        
+        let msg : JSQMessage = self.demoData.messages[indexPath.item]
+        
+        if !msg.isMediaMessage {
+            if msg.senderId == self.senderId {
+                cell.textView.textColor = UIColor.blackColor()
+            }
+            else {
+                cell.textView.textColor = UIColor.whiteColor()
+            }
+            
+            // TODO: lignes suivantes.
+            cell.textView.linkTextAttributes = [ NSForegroundColorAttributeName : cell.textView.textColor ] //,
+//                NSUnderlineStyleAttributeName : (.NSUnderlineStyleSingle | .NSUnderlinePatternSolid) ]
+        }
+        
+        return cell
+    }
+    
+    // ----------------------------------------------------------------------
+    // MARK: - UICollectionView DataSource
+    // ----------------------------------------------------------------------
     
     
 }
