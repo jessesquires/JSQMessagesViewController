dad900b1858c7a27cf0b2caf552da3cecc2e46f1
Jesse Squires <jesse.d.squires@gmail.com>
2015-09-02 23:27:01 -0700

    clean up / tweak demo changes from #1176

--- JSQMessagesDemo/TableViewController.m
+++ JSQMessagesDemo/TableViewController.m
@@ -38,19 +38,23 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 3;
+    return 4;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    if (section == 0) {
-        return 3;
+    switch (section) {
+        case 0:
+            return 2;
+        case 1:
+            return 2;
+        case 2:
+            return 1;
+        case 3:
+            return 1;
+        default:
+            return 0;
     }
-    if (section == 2) {
-        return 1;
-    }
-    
-    return 2;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -61,7 +65,7 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     }
-    
+
     if (indexPath.section == 0) {
         switch (indexPath.row) {
             case 0:
@@ -69,9 +73,6 @@
                 break;
             case 1:
                 cell.textLabel.text = @"Push programmatically";
-                break;
-            case 2:
-                cell.textLabel.text = @"Push 2-levels programmatically";
                 break;
         }
     }
@@ -92,7 +93,14 @@
                 break;
         }
     }
-    
+    else if (indexPath.section == 3) {
+        switch (indexPath.row) {
+            case 0:
+                cell.textLabel.text = @"Push view 2 levels";
+                break;
+        }
+    }
+
     return cell;
 }
 
@@ -103,6 +111,8 @@
             return @"Presentation";
         case 2:
             return @"Demo options";
+        case 3:
+            return @"Other testing";
         default:
             return nil;
     }
@@ -110,7 +120,7 @@
 
 - (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section
 {
-    return (section == [tableView numberOfSections] - 1) ? @"Copyright © 2014\nJesse Squires\nMIT License" : nil;
+    return (section == 2) ? @"Copyright © 2015\nJesse Squires\nMIT License" : nil;
 }
 
 #pragma mark - Table view delegate
@@ -128,14 +138,6 @@
                 [self.navigationController pushViewController:vc animated:YES];
             }
                 break;
-            case 2:
-            {
-                UIViewController *blank = [[UIViewController alloc] init];
-                [self.navigationController pushViewController:blank animated:NO];
-                [self.navigationController.view setBackgroundColor:[UIColor whiteColor]];
-                DemoMessagesViewController *vc = [DemoMessagesViewController messagesViewController];
-                [self.navigationController pushViewController:vc animated:YES];
-            }
         }
     }
     else if (indexPath.section == 1) {
@@ -158,6 +160,19 @@
             case 0:
                 [self performSegueWithIdentifier:@"SegueToSettings" sender:self];
                 break;
+        }
+    }
+    else if (indexPath.section == 3) {
+        switch (indexPath.row) {
+            case 0:
+            {
+                UIViewController *blank = [[UIViewController alloc] initWithNibName:nil bundle:nil];
+                blank.title = @"Blank";
+                blank.view.backgroundColor = [UIColor lightGrayColor];
+                [self.navigationController pushViewController:blank animated:NO];
+                DemoMessagesViewController *vc = [DemoMessagesViewController messagesViewController];
+                [self.navigationController pushViewController:vc animated:YES];
+            }
         }
     }
 }
