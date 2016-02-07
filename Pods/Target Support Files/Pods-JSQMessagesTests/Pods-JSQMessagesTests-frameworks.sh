a907443d259b3bb4b638fa65ce5cedf98317026f
Dan Leonard <dan.leonard@getweave.com>
2016-02-06 21:12:59 -0700

    add `use_frameworks!` and update pods to solve issue #1284

--- Pods/Target Support Files/Pods-JSQMessagesTests/Pods-JSQMessagesTests-frameworks.sh
+++ Pods/Target Support Files/Pods-JSQMessagesTests/Pods-JSQMessagesTests-frameworks.sh
@@ -82,3 +82,10 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-JSQMessagesTests/JSQSystemSoundPlayer.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-JSQMessagesTests/JSQSystemSoundPlayer.framework"
+fi
