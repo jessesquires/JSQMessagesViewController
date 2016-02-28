d2c73ee7a968fc3fb19b4dc73b3dd228bc71e680
Dan Leonard <dan.leonard@getweave.com>
2016-02-28 16:54:43 -0700

    Changed the name so that there will be less confusion 🤔😤😫😡

--- DemoSwift/JSwiftQMessages/Pods/Target Support Files/Pods-JSwiftQMessagesTests/Pods-JSwiftQMessagesTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JSwiftQMessagesTests : NSObject
-@end
-@implementation PodsDummy_Pods_JSwiftQMessagesTests
-@end
--- DemoSwift/JSwiftQMessages/Pods/Target Support Files/Pods-JSwiftQMessagesUITests/Pods-JSwiftQMessagesUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JSwiftQMessagesUITests : NSObject
-@end
-@implementation PodsDummy_Pods_JSwiftQMessagesUITests
-@end
