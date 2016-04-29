87b44a6bed8b603d1b4e8008162ecdd2d4b2b4ca
Dan Leonard <dan.leonard@getweave.com>
2016-04-29 15:59:14 -0600

    no message

--- SwiftDemo/MacmeSwiftChat/MacmeSwiftChatTests/MacmeSwiftChatTests.swift
+++ SwiftDemo/MacmeSwiftChat/MacmeSwiftChatTests/MacmeSwiftChatTests.swift
@@ -13,17 +13,16 @@ class MacmeSwiftChatTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let image = UIImage(imageLiteral: "NiceSelfi")
+        XCTAssertNotNil(image, "This should not be nil")
+        
     }
     
     func testPerformanceExample() {
