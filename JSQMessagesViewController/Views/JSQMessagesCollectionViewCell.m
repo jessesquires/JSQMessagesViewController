f4fb6799d7e31b0e7e77f734b94fce914e2d120f
Francisco Amado <franciscoamado@live.com.pt>
2016-06-07 12:56:21 +0100

    Fix issue #1583: Don't highlight cell outside message bubble (when long press)
    In summary, overriden "touchesBegan" gaining all touch control of the JSQMessagesCollectionViewCell touches

--- JSQMessagesViewController/Views/JSQMessagesCollectionViewCell.m
+++ JSQMessagesViewController/Views/JSQMessagesCollectionViewCell.m
@@ -211,12 +211,14 @@ static NSMutableSet *jsqMessagesCollectionViewCellActions = nil;
 - (void)setHighlighted:(BOOL)highlighted
 {
     [super setHighlighted:highlighted];
+    self.avatarImageView.highlighted = highlighted;
     self.messageBubbleImageView.highlighted = highlighted;
 }
 
 - (void)setSelected:(BOOL)selected
 {
     [super setSelected:selected];
+    self.avatarImageView.highlighted = selected;
     self.messageBubbleImageView.highlighted = selected;
 }
 
@@ -230,6 +232,9 @@ static NSMutableSet *jsqMessagesCollectionViewCellActions = nil;
     }
 
     return [super respondsToSelector:aSelector];
+}
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
 }
 
 - (void)forwardInvocation:(NSInvocation *)anInvocation
@@ -368,7 +373,7 @@ static NSMutableSet *jsqMessagesCollectionViewCellActions = nil;
         return CGRectContainsPoint(self.messageBubbleContainerView.frame, touchPt);
     }
     
-    return YES;
+    return NO;
 }
 
 @end
