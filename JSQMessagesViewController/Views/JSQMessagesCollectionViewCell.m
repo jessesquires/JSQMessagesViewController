5990da8a8676fa8577324a9a775975891250871d
Francisco Amado <franciscoamado@live.com.pt>
2016-06-07 16:52:15 +0100

    Fix issue #1583: added missing touch handling methods required by overriding "touchesBegan"

--- JSQMessagesViewController/Views/JSQMessagesCollectionViewCell.m
+++ JSQMessagesViewController/Views/JSQMessagesCollectionViewCell.m
@@ -237,6 +237,15 @@ static NSMutableSet *jsqMessagesCollectionViewCellActions = nil;
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
 }
 
+-(void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+}
+
+-(void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+}
+
+-(void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+}
+
 - (void)forwardInvocation:(NSInvocation *)anInvocation
 {
     if ([jsqMessagesCollectionViewCellActions containsObject:NSStringFromSelector(anInvocation.selector)]) {
