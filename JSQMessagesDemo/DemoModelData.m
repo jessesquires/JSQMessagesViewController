e77a45e6be85cc15d6e7404929ede81127901094
Alexei Gridnev <alx.gridnev@gmail.com>
2016-10-06 23:42:28 +0300

    Creating deep copies for extra messages in demo model data

--- JSQMessagesDemo/DemoModelData.m
+++ JSQMessagesDemo/DemoModelData.m
@@ -135,9 +135,12 @@
      *  Setting to load extra messages for testing/demo
      */
     if ([NSUserDefaults extraMessagesSetting]) {
-        NSArray *copyOfMessages = [self.messages copy];
-        for (NSUInteger i = 0; i < 4; i++) {
-            [self.messages addObjectsFromArray:copyOfMessages];
+        NSArray *copyOfMessages = [NSKeyedUnarchiver unarchiveObjectWithData:
+            [NSKeyedArchiver archivedDataWithRootObject:self.messages]];
+        [self.messages addObjectsFromArray:copyOfMessages];
+        for (NSUInteger i = 0; i < 3; i++) {
+            [self.messages addObjectsFromArray:[NSKeyedUnarchiver unarchiveObjectWithData:
+                [NSKeyedArchiver archivedDataWithRootObject:copyOfMessages]]];
         }
     }
     
