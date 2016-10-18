b72cd05f24376d6e1d7e64124819c9a38ed09dd3
Jesse Squires <jesse.d.squires@gmail.com>
2016-10-18 07:51:21 -0700

    update examples

--- SwiftExample/Pods/Target Support Files/Pods-SwiftExampleTests/Pods-SwiftExampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftExampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftExampleTests
-@end
--- SwiftExample/Pods/Target Support Files/Pods-SwiftExampleTests/Pods-SwiftExampleTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_SwiftExampleTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_SwiftExampleTestsVersionString[];
-
