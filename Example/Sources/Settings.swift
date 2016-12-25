3aaab30aef5b53bc57634ac083dc61dde3453ce6
Jesse Squires <jesse.d.squires@gmail.com>
2016-12-25 17:03:42 -0500

    finish porting demo settings

---
+++ Example/Sources/Settings.swift
@@ -0,0 +1,134 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://cocoadocs.org/docsets/JSQMessagesViewController
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQMessagesViewController
+//
+//
+//  License
+//  Copyright © 2014-present Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+import Foundation
+
+
+fileprivate enum Key: String {
+    case presentModal
+
+    case extraMessages
+    case longMessage
+    case emptyMessages
+
+    case incomingAvatars
+    case outgoingAvatars
+    case accessoryForMedia
+
+    case springyBubbles
+}
+
+
+extension UserDefaults {
+    fileprivate func bool(key: Key) -> Bool {
+        return bool(forKey: key.rawValue)
+    }
+
+    fileprivate func set(_ value: Bool, key: Key) {
+        set(value, forKey: key.rawValue)
+    }
+}
+
+
+final class Settings {
+    fileprivate let defaults = UserDefaults.standard
+}
+
+
+extension Settings {
+    var presentModally: Bool {
+        get {
+            return defaults.bool(key: .presentModal)
+        }
+        set {
+            defaults.set(newValue, key: .presentModal)
+        }
+    }
+}
+
+
+extension Settings {
+    var extraMessages: Bool {
+        get {
+            return defaults.bool(key: .extraMessages)
+        }
+        set {
+            defaults.set(newValue, key: .extraMessages)
+        }
+    }
+
+    var longMessage: Bool {
+        get {
+            return defaults.bool(key: .longMessage)
+        }
+        set {
+            defaults.set(newValue, key: .longMessage)
+        }
+    }
+
+    var emptyMessages: Bool {
+        get {
+            return defaults.bool(key: .emptyMessages)
+        }
+        set {
+            defaults.set(newValue, key: .emptyMessages)
+        }
+    }
+}
+
+
+extension Settings {
+    var incomingAvatars: Bool {
+        get {
+            return defaults.bool(key: .incomingAvatars)
+        }
+        set {
+            defaults.set(newValue, key: .incomingAvatars)
+        }
+    }
+
+    var outgoingAvatar: Bool {
+        get {
+            return defaults.bool(key: .outgoingAvatars)
+        }
+        set {
+            defaults.set(newValue, key: .outgoingAvatars)
+        }
+    }
+
+    var accessoryForMedia: Bool {
+        get {
+            return defaults.bool(key: .accessoryForMedia)
+        }
+        set {
+            defaults.set(newValue, key: .accessoryForMedia)
+        }
+    }
+}
+
+
+extension Settings {
+    var springyBubbles: Bool {
+        get {
+            return defaults.bool(key: .springyBubbles)
+        }
+        set {
+            defaults.set(newValue, key: .springyBubbles)
+        }
+    }
+}
--- Example/Sources/SettingsViewController.swift
+++ Example/Sources/SettingsViewController.swift
@@ -20,7 +20,76 @@ import UIKit
 
 final class SettingsViewController: UITableViewController {
 
+    let settings = Settings()
+
+    // MARK: Outlets
+
+    @IBOutlet weak var modalSwitch: UISwitch!
+
+    @IBOutlet weak var extraMessagesSwitch: UISwitch!
+    @IBOutlet weak var longMessageSwitch: UISwitch!
+    @IBOutlet weak var emptyMessagesSwitch: UISwitch!
+
+    @IBOutlet weak var incomingAvatarSwitch: UISwitch!
+    @IBOutlet weak var outgoingAvatarSwitch: UISwitch!
+    @IBOutlet weak var accessoryForMediaSwitch: UISwitch!
+
+    @IBOutlet weak var springBubblesSwitch: UISwitch!
+
+    // MARK: View lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        modalSwitch.isOn = settings.presentModally
+
+        extraMessagesSwitch.isOn = settings.extraMessages
+        longMessageSwitch.isOn = settings.longMessage
+        emptyMessagesSwitch.isOn = settings.emptyMessages
+
+        incomingAvatarSwitch.isOn = settings.incomingAvatars
+        outgoingAvatarSwitch.isOn = settings.outgoingAvatar
+        accessoryForMediaSwitch.isOn = settings.accessoryForMedia
+
+        springBubblesSwitch.isOn = settings.springyBubbles
+    }
+
+    // MARK: Actions
+
+    @IBAction func didFlipSwitch(_ sender: UISwitch) {
+        let value = sender.isOn
+
+        switch sender {
+        case modalSwitch:
+            settings.presentModally = value
+
+        case extraMessagesSwitch:
+            settings.extraMessages = value
+
+        case longMessageSwitch:
+            settings.longMessage = value
+
+        case emptyMessagesSwitch:
+            settings.emptyMessages = value
+
+        case incomingAvatarSwitch:
+            settings.incomingAvatars = value
+
+        case outgoingAvatarSwitch:
+            settings.outgoingAvatar = value
+
+        case accessoryForMediaSwitch:
+            settings.accessoryForMedia = value
+
+        case springBubblesSwitch:
+            settings.springyBubbles = value
+
+        default:
+            fatalError()
+        }
     }
 }
