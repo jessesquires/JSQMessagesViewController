91b5d3043e5472a11dd48208c64791ea7ed8ce15
Jesse Squires <jesse.d.squires@gmail.com>
2016-12-25 15:04:24 -0500

    add framework

---
+++ Example/Sources/MessagesViewController.swift
@@ -0,0 +1,38 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://cocoadocs.org/docsets/JSQMessagesViewController
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQMessagesViewController
+//
+//
+//  License
+//  Copyright © 2014-present Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+
+import UIKit
+import JSQMessagesViewController
+
+
+final class MessagesViewController: JSQMessagesViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func senderId() -> String {
+        return "id"
+    }
+
+    override func senderDisplayName() -> String {
+        return "jsq"
+    }
+
+}
